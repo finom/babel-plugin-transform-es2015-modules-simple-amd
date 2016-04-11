1fb48ca3e288789a9f531aaeb0f3a0c40c9f3a9d
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2016-04-11 15:31:45 +0300

    add missing files to git index

---
+++ test/fixtures/basic/actual.js
@@ -0,0 +1,8 @@
+import a from '/path/to/a';
+import '/path/to/b';
+import c from '/path/to/c';
+import '/path/to/d';
+import {e, f as g, h, i as j} from '/path/to/e';
+import {k} from '/path/to/k';
+doSomething();
+export default x + y;
---
+++ test/fixtures/basic/expected.js
@@ -0,0 +1,11 @@
+'use strict';
+
+define(['/path/to/a', '/path/to/c', '/path/to/e', '/path/to/k', '/path/to/b', '/path/to/d'], function (a, c, _pathToE, _pathToK) {
+  var e = _pathToE.e;
+  var g = _pathToE.f;
+  var h = _pathToE.h;
+  var j = _pathToE.i;
+  var k = _pathToK.k;
+  doSomething();
+  return x + y;
+});
